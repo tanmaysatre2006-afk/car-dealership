473639670c2b310bb9ecc4383b06a4fb77e7ae95
tanmaysatre2006-afk <tanmaysatre2006@gmail.com>
2026-09-19 14:29:58 +0530

    Replace React.StrictMode with StrictMode import

--- main.jsx
+++ main.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
